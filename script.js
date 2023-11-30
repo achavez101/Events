b5a69efa6ecad02a662b314939018bd5fd0b8482
achavez101 <amairanygchavez@gmail.com>
2023-11-30 13:48:53 -0800

    event object section

--- script.js
+++ script.js
@@ -1,6 +1,98 @@
 
+// video 87
+
+// video 86
+// const logo = document.querySelector('img');
+
+//function onClick(e) {
+
+    // Target
+    // console.log(e.target);
+    // Current Target
+    // console.log(e.currentTarget);
+    // Type
+    // console.log(e.type);
+    // Time Stamp
+    // console.log(e.timeStamp);
+    // client X - horizontal
+    // console.log(e.clientX);
+    // client Y - vertical
+    // console.log(e.clientY);
+    // offset X
+    // console.log(e.offsetX);
+    // offset Y
+    // console.log(e.offsetY);
+    // console.log(e.pageX);
+    // console.log(e.pageY);
+    // console.log(e.screenX);
+    // console.log(e.screenY);
 
 
+    // can use as well to change elements
+//     console.log(e.currentTarget);
+//     e.target.style.backgroundColor = 'black';
+//}
+// function onDrag(e) {
+//     document.querySelector('h1').textContent = `X ${e.clientX} Y ${e.clientY}`;
+// }
+
+// logo.addEventListener('click', onClick);
+// logo.addEventListener('drag', onDrag);
+
+
+// document.body.addEventListener('click', function (e) {
+//     console.log(e.target);
+//     console.log(e.currentTarget);
+// });
+
+// document.querySelector('a').addEventListener('click', function(e){
+//     e.preventDefault();
+//     console.log('Link was clicked');
+// })
+
+// video 85
+
+// const logo = document.querySelector('img');
+// const onClick = () => console.log('click event');
+// const onDoubleClick = () => {
+    
+//     //Toggle Example
+    
+//     if(document.body.style.backgroundColor !== 'purple'){
+//         document.body.style.backgroundColor = 'purple';
+//         document.body.style.color = 'white';
+//     } else if (document.body.style.backgroundColor === 'purple'){
+//         document.body.style.backgroundColor = 'white';
+//         document.body.style.color = 'black';
+//     }
+    
+// };
+// const onRightClick = () => console.log('right click event');
+// const onMouseDown = () => console.log('mouse down event');
+// const onMouseUp = () => console.log('mouse up event');
+// const onMouseWheel = () => console.log('mouse wheel event'); //doesn't detect my bluetooth mouse
+// const onMouseOver = () => console.log('mouse over event'); //doesn't detect my bluetooth mouse
+// const onMouseOut = () => console.log('mouse out event'); //doesn't detect my bluetooth mouse
+// const onDragStart = () => console.log('drag start event'); //doesn't detect my bluetooth mouse
+// const onDrag = () => console.log('drag event'); //doesn't detect my bluetooth mouse
+// const onDragEnd = () => console.log('drag end event'); //doesn't detect my bluetooth mouse
+
+
+// //event listeners
+// logo.addEventListener('click', onClick);
+// logo.addEventListener('dblclick', onDoubleClick);
+// logo.addEventListener('contextmenu', onRightClick);
+// logo.addEventListener('mousedown', onMouseDown);
+// logo.addEventListener('mouseup', onMouseUp);
+// logo.addEventListener('wheel', onMouseWheel); // i don't have a wheel
+// logo.addEventListener('mouseover', onMouseOver);
+// logo.addEventListener('mouseout', onMouseOut);
+// logo.addEventListener('dragstart', onDragStart);
+// logo.addEventListener('drag', onDrag);
+// logo.addEventListener('dragend', onDragEnd);
+
+
+//video 83-84
 
 // const clearBtn = document.querySelector('#clear');
 
@@ -40,8 +132,3 @@
 // while (itemList.firstChild) {
 //     itemList.removeChild(itemList.firstChild);
 // }
-
-
-
-
-
