bf179c199b7bd7dda4b59e922d4ab4abc9dbb1be
achavez101 <amairanygchavez@gmail.com>
2023-12-01 05:58:04 -0800

    section complete

---
+++ Window-Event/script.js
@@ -0,0 +1,51 @@
+
+// window.onload = function () {
+//     document.querySelector('h1').textContent = 'Hello World';
+// }
+
+
+// window.addEventListener(
+//     'load',
+//     () => (document.querySelector('h1').textContent = 'Hello World')
+// );
+
+window.addEventListener('load', () => console.log('Page Loaded'));
+window.addEventListener('DOMContentLoaded', () => console.log('DOM Loaded'));
+
+console.log('Run me');
+
+document.querySelector('h1').innerText = 'Hello World';
+
+window.addEventListener('resize', () => {
+    document.querySelector(
+        'h1'
+    ).innerText = `Resized to ${window.innerWidth} x ${window.innerHeight}`;
+});
+
+// scrolls don't work on mac with trackpad
+
+window.addEventListener('scroll', () => {
+    console.log(`Scrolled: ${window.scrollX} x ${window.scrollY}`);
+
+    if (window.scrollY > 70) {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.backgroundColor = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.backgroundColor = 'black';
+    }
+});
+
+window.addEventListener('focus', () => {
+    document.querySelectorAll('p').forEach((p) => {
+        p.style.color = 'blue';
+    });
+});
+
+window.addEventListener('blur', () => {
+    document.querySelectorAll('p').forEach((p) => {
+        p.style.color = 'black';
+    });
+});
+
+
